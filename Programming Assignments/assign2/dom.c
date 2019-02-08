c965e211581893f88c5427775474fdafb3c83637
niskarsh <niskarshk@gmail.com>
2019-02-08 22:47:20 +0530

    Input set with pointers

--- Programming Assignments/assign2/dom.c
+++ Programming Assignments/assign2/dom.c
@@ -4,22 +4,14 @@
 int main () {
 
     int n, i, j, **point;
-
     scanf("%d", &n);
-
     point = (int**) malloc(n*sizeof(int*));
     
     for(i=0;i<n;i++) {
         point[i] = (int*) malloc(2*sizeof(int));
         fflush(stdout);
-
-        printf("I is %d\n", i);
-        scanf("%d %d", &point[i][0], &point[i][1]);
-        // fflush(stdin);
+        scanf("%d %d", (*(point+i)+0), (*(point+i)+1));
     }
-    for(i=0;i<n;i++) {
-        fflush(stdout);
-        printf("enters %d and %d \n", point[i][0], point[i][1]);
-    }
+    
     return 0;
 }
