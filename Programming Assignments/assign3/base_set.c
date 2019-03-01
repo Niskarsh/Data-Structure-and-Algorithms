d9f9af98aecaa28fecf4d12ef9f6404a7856107d
niskarsh <niskarshk@gmail.com>
2019-03-01 18:13:32 +0530

    Bitwise operators used, code accepted

--- Programming Assignments/assign3/base_set.c
+++ Programming Assignments/assign3/base_set.c
@@ -9,9 +9,7 @@ typedef struct {
 
 int power (int, int);
 void base_set (sum*, int);
-int nHighest (int);
-int combination (int, int);
-int factorial (int);
+void sum_set (sum*, int, int);
 
 int main () {
     int N, i, j, p;
@@ -35,48 +33,59 @@ int main () {
         }
         printf("\n");
     }
-    // for(i =0; i<N; i++) {
-    //     for (j=0; j<power(2, sArray[i].n); j++) {
-    //         printf("%d\n", *((sArray[i].s)+j));
-    //     }
-    // }
     return 0;
 }
 
 void base_set (sum* sArray, int p) {
 
-    int i, j, k, len;
-    len = power(2, sArray[p].n);
+    int dis=2, cnt=3;
     if (sArray[p].n==1) {
         *(sArray[p].base_set+0) = *(sArray[p].s+1);
     } else if (sArray[p].n==2) {
         *(sArray[p].base_set+0) = *(sArray[p].s+1);
         *(sArray[p].base_set+1) = *(sArray[p].s+2);
+    } else {
+        *(sArray[p].base_set+0) = *(sArray[p].s+1);
+        *(sArray[p].base_set+1) = *(sArray[p].s+2);
+        while (dis<sArray[p].n) {
+            sum_set (sArray, p, dis);
+            while (*(sArray[p].s+cnt)==-1) {
+                cnt++;
+            }
+            *(sArray[p].base_set+dis) = *(sArray[p].s+cnt);
+            ++dis;
+        }
+
     }
-    for (i=0; i<len; i++) {
+}
 
+void sum_set (sum* sArray, int p, int dis) {
+    int i, j, pow, sum=0;
+    pow = power (2, dis);
+    for (i=0; i<pow; i++) {
+        sum=0;
+        if ( i & (1<<(dis-1))) {
+            for (j=0; j<dis; j++) {
+                if( i & (1<<j)) {
+                    sum = sum + *(sArray[p].base_set+j);
+                }
+            }
+            j=3;
+            int powe = power(2, sArray[p].n);
+            while (j<powe){
+                if (*(sArray[p].s+j)==sum) {
+                    *(sArray[p].s+j) = -1;
+                    break;
+                }
+                ++j;
+            }
+        }
+        
 
+        
+        
     }
 }
-
-
-int nHighest (int n) {
-    
-}
-
-int combination (int n, int k) {
-    return (factorial(n)/(factorial(k)*factorial(n-k)));
-}
-
-int factorial (int n) {
-    int i, f=1;
-    for (i=2;i<=n;i++) {
-        f*=i;
-    }
-    return f;
-
-}
-
 
 int power (int b, int exp) {
     int p=1;
