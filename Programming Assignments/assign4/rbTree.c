80af9284e24e9a41f721bf26eefbce3a98127c08
niskarsh <niskarshk@gmail.com>
2019-03-30 19:53:27 +0530

    struct created

---
+++ Programming Assignments/assign4/rbTree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node {
+    int n;
+    struct node *parent;
+    struct node *lchild;
+    struct node *rchild;
+    char color;
+};
+
+int main() {
+    
+
+
+    return 0;
+}
